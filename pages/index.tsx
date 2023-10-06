a7d070120e1273705deef55b835b313c19b76786
noriyotcp <noriyo.akita@gmail.com>
2023-10-07 02:12:22 +0900

    p106. Add Content and Post types

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,26 @@ const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
 
+export type Content =
+  | {
+      type: "paragraph" | "quote" | "heading_2" | "heading_3";
+      text: string | null;
+    }
+  | {
+      type: "code";
+      text: string | null;
+      language: string | null;
+    };
+
+export type Post = {
+  id: string;
+  title: string | null;
+  slug: string | null;
+  createdTs: string | null;
+  lastEditedTs: string | null;
+  content: Content[];
+};
+
 export const getStaticProps: GetStaticProps<{}> = async () => {
   const database = await notion.databases.query({
     database_id: process.env.NOTION_DATABASE_ID || "",
