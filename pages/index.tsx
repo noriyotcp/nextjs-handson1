bb66f0e7c64bde725adc28c937c82a7f85bf2d48
noriyotcp <noriyo.akita@gmail.com>
2023-10-07 23:46:09 +0900

    p116. Display createdTs and lastEditedTs in Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import {
 } from "@notionhq/client/build/src/api-endpoints";
 import { GetStaticProps, NextPage } from "next";
 import styles from "../styles/Home.module.css";
+import dayjs from "dayjs";
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
@@ -150,6 +151,16 @@ const Home: NextPage<StaticProps> = ({ post }) => {
     <div className={styles.wrapper}>
       <div className={styles.post}>
         <h1 className={styles.title}>{post.title}</h1>
+        <div className={styles.timestampWrapper}>
+          <div>
+            <div className={styles.timestamp}>
+              作成日時: {dayjs(post.createdTs).format("YYYY-MM-DD HH:mm:ss")}
+            </div>
+            <div className={styles.timestamp}>
+              更新日時: {dayjs(post.lastEditedTs).format("YYYY-MM-DD HH:mm:ss")}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
