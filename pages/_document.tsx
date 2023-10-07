ff0b3dc44ec2604f28001d3dee288a30a5794a50
noriyotcp <noriyo.akita@gmail.com>
2023-10-07 23:32:58 +0900

    p114. Include Noto Sans JP

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
+export default function Index() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href={
+            "https://fonts.googleapis.com/css2" +
+            "?family=Noto+Sans+JP:wght@400;500;700" +
+            "&display=swap"
+          }
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
