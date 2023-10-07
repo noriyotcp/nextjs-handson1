b57af7465055ad9906ce6ce4cc4477d3bf74c696
noriyotcp <noriyo.akita@gmail.com>
2023-10-08 01:09:58 +0900

    p134. Extract code to getPostContents()

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,6 +38,53 @@ type StaticProps = {
   posts: Post[];
 };
 
+async function getPostContents(post: Post): Promise<Content[]> {
+  const blockResponse = await notion.blocks.children.list({
+    block_id: post.id,
+  });
+
+  const contents: Content[] = [];
+  blockResponse.results.forEach((block) => {
+    if (!("type" in block)) {
+      return;
+    }
+    switch (block.type) {
+      case "paragraph":
+        contents.push({
+          type: "paragraph",
+          text: block.paragraph?.rich_text[0]?.plain_text ?? null,
+        });
+        break;
+      case "heading_2":
+        contents.push({
+          type: "heading_2",
+          text: block.heading_2?.rich_text[0]?.plain_text ?? null,
+        });
+        break;
+      case "heading_3":
+        contents.push({
+          type: "heading_3",
+          text: block.heading_3?.rich_text[0]?.plain_text ?? null,
+        });
+        break;
+      case "quote":
+        contents.push({
+          type: "quote",
+          text: block.quote?.rich_text[0]?.plain_text ?? null,
+        });
+        break;
+      case "code":
+        contents.push({
+          type: "code",
+          text: block.code?.rich_text[0]?.plain_text ?? null,
+          language: block.code?.language,
+        });
+    }
+  });
+
+  return contents;
+}
+
 export async function getPosts(slug?: string): Promise<Post[]> {
   let database: QueryDatabaseResponse | undefined = undefined;
   if (slug) {
@@ -77,88 +124,41 @@ export async function getPosts(slug?: string): Promise<Post[]> {
   }
 
   const posts: Post[] = [];
-  const blockResponses = await Promise.all(
-    database.results.map((page) => {
-      return notion.blocks.children.list({
-        block_id: page.id,
+  await Promise.all(
+    database.results.map(async (page) => {
+      if (!("properties" in page)) {
+        return;
+      }
+      let title: string | null = null;
+      if (page.properties["Name"].type === "title") {
+        const titleRes = page.properties["Name"]
+          .title as RichTextItemResponse[];
+        title = titleRes[0]?.plain_text ?? null;
+      }
+      let slug: string | null = null;
+      if (page.properties["Slug"].type === "rich_text") {
+        const slugRes = page.properties["Slug"]
+          .rich_text as RichTextItemResponse[];
+        slug = slugRes[0]?.plain_text ?? null;
+      }
+      const contents = await getPostContents({
+        id: page.id,
+        title,
+        slug,
+        createdTs: (page as PageObjectResponse).created_time,
+        lastEditedTs: (page as PageObjectResponse).last_edited_time,
+        content: [],
+      });
+      posts.push({
+        id: page.id,
+        title,
+        slug,
+        createdTs: (page as PageObjectResponse).created_time,
+        lastEditedTs: (page as PageObjectResponse).last_edited_time,
+        content: contents,
       });
     })
   );
-
-  database.results.forEach((page, index) => {
-    if (!("properties" in page)) {
-      return {
-        props: {
-          post: {
-            id: page.id,
-            title: null,
-            slug: null,
-            createdTs: null,
-            lastEditedTs: null,
-            content: [],
-          },
-        },
-      };
-    }
-    let title: string | null = null;
-    if (page.properties["Name"].type === "title") {
-      const titleRes = page.properties["Name"].title as RichTextItemResponse[];
-      title = titleRes[0]?.plain_text ?? null;
-    }
-    let slug: string | null = null;
-    if (page.properties["Slug"].type === "rich_text") {
-      const slugRes = page.properties["Slug"]
-        .rich_text as RichTextItemResponse[];
-      slug = slugRes[0]?.plain_text ?? null;
-    }
-    const blocks = blockResponses[index];
-    const contents: Content[] = [];
-    blocks.results.forEach((block) => {
-      if (!("type" in block)) {
-        return;
-      }
-      switch (block.type) {
-        case "paragraph":
-          contents.push({
-            type: "paragraph",
-            text: block.paragraph?.rich_text[0]?.plain_text ?? null,
-          });
-          break;
-        case "heading_2":
-          contents.push({
-            type: "heading_2",
-            text: block.heading_2?.rich_text[0]?.plain_text ?? null,
-          });
-          break;
-        case "heading_3":
-          contents.push({
-            type: "heading_3",
-            text: block.heading_3?.rich_text[0]?.plain_text ?? null,
-          });
-          break;
-        case "quote":
-          contents.push({
-            type: "quote",
-            text: block.quote?.rich_text[0]?.plain_text ?? null,
-          });
-          break;
-        case "code":
-          contents.push({
-            type: "code",
-            text: block.code?.rich_text[0]?.plain_text ?? null,
-            language: block.code?.language,
-          });
-      }
-    });
-    posts.push({
-      id: page.id,
-      title,
-      slug,
-      createdTs: (page as PageObjectResponse).created_time,
-      lastEditedTs: (page as PageObjectResponse).last_edited_time,
-      content: contents,
-    });
-  });
 
   return posts;
 }
