46c0413f3336710e1e549625535d4076eda15add
noriyotcp <noriyo.akita@gmail.com>
2023-10-08 00:59:09 +0900

    p130. Create an individual post page

---
+++ pages/post/[slug].tsx
@@ -0,0 +1,22 @@
+import { GetStaticPaths } from "next";
+import { getPosts } from "../index";
+
+type StaticPathsParams = {
+  slug: string;
+};
+
+export const getStaticPaths: GetStaticPaths<StaticPathsParams> = async () => {
+  const posts = await getPosts();
+  const paths: {
+    params: { slug: string };
+  }[] = [];
+
+  posts.forEach((post) => {
+    const slug = post.slug;
+    if (slug) {
+      paths.push({ params: { slug } });
+    }
+  });
+
+  return { paths, fallback: "blocking" };
+};
