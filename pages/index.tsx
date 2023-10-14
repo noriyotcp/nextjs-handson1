eb6743e5e0a6559b327f018a92276bc6dfc71d5a
noriyotcp <noriyo.akita@gmail.com>
2023-10-14 23:34:19 +0900

    p137. Add link to a show page to post title with Link component

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import styles from "../styles/Home.module.css";
 import dayjs from "dayjs";
 import prism from "prismjs";
 import { useEffect } from "react";
+import Link from "next/link";
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
@@ -191,7 +192,11 @@ const Home: NextPage<StaticProps> = ({ posts }) => {
     <div className={styles.wrapper}>
       {posts.map((post) => (
         <div className={styles.post} key={post.id}>
-          <h1 className={styles.title}>{post.title}</h1>
+          <h1 className={styles.title}>
+            <Link href={`/post/${encodeURIComponent(post.slug ?? "")}`}>
+              {post.title}
+            </Link>
+          </h1>
           <div className={styles.timestampWrapper}>
             <div>
               <div className={styles.timestamp}>
